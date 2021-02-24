d57c20afac110f3873dab85bad61a860c7a9ae95
Adrian De Lisle <adriandelisle@gmail.com>
2021-02-24 00:08:11 -0800

    Initial commit of placeholder nextjs site

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  tabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+}
