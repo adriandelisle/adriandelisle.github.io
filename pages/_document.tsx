d0832b6a2844b40bdfce1b789996eeb24ab4eebc
Adrian De Lisle <adriandelisle@gmail.com>
2021-02-26 23:37:53 -0800

    Add cloudflare web analytics

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <script
+            defer
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "ada54e0a637048bfb83d4ecfef0d1d95", "spa": true}'
+          />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
